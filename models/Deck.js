6affd27233003adcfdf4ad29791a20fd5c455638
Zion Griffin <defendingmymaininbox@gmail.com>
2024-01-09 21:10:21 -0500

    Finished models (I think)

--- models/Deck.js
+++ models/Deck.js
@@ -5,6 +5,24 @@ class Deck extends Model {}
 
 Deck.init(
     {
-        
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'Unnamed Deck',
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'deck',
     }
-)
+);
+
+module.exports = Deck;
