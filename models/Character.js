70e349cc8b1fd90dad864426137adc17b87087da
Zion Griffin <defendingmymaininbox@gmail.com>
2024-01-09 19:59:06 -0500

    Working on routes still

---
+++ models/Character.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Character extends Model {}
+
+Character.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        attack_points: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        defense_points: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'character',
+    }
+);
+
+module.exports = Character;
