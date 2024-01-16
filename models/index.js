9b6bae4200ebb973968bb105de64a18b25773a67
Ashley <ashleyh5440@gmail.com>
2024-01-16 18:48:12 -0500

    edited game.handlebars

--- models/index.js
+++ models/index.js
@@ -2,9 +2,13 @@ const User = require('./User');
 const Deck = require('./Deck');
 const Character = require('./Character');
 
-Deck.hasMany(Character, { foreignKey: 'deck_id' });
-Character.belongsTo(Deck, {
-    foreignKey: 'deck_id',
+// Deck.hasMany(Character, { foreignKey: 'deck_id' });
+// Character.belongsTo(Deck, {
+//     foreignKey: 'deck_id',
+// })
+
+User.hasOne(Deck {
+
 })
 
 
