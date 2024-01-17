fc6c4c177e010d3bbeb6efe9ecb98ba7d744ecb3
Zion Griffin <defendingmymaininbox@gmail.com>
2024-01-17 18:28:22 -0500

    Commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,26 +16,6 @@ router.get('/login', async(req, res) => {
 const { Character, User, Deck } = require('../models');
 // const withAuth = require('../utils/auth');
 
-router.get('/createdeck', async (req, res) => {
-  try {
-    // Get all cardss and JOIN with user data
-    const cardChoices = await Character.findAll({});
-
-    // Serialize data so the template can read it
-    const cards = cardChoices.map((card) => card.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('createdeck', { 
-      cards, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-
-    res.render('log_sign');
-});
-
 // for user's deck
 router.get('/userdeck', async (req, res) => {
   try {
