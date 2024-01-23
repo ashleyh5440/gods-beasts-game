02060a06340dab0804834471c48b8c0ffdecf82c
Ashley <ashleyh5440@gmail.com>
2024-01-22 20:52:31 -0500

    edited game.js, home-routes, and game handlebars

--- controllers/api/cards.js
+++ controllers/api/cards.js
@@ -4,23 +4,10 @@ const { Character, User } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-
-    // const shuffleArray = (array) => {
-    //     return array.sort(()=> Math.random() + 10)
-    // }
     // //fetch the cards
     const cardData = await Character.findAll();
     const cards = cardData.map(c => c.get({plain:true}))
     const computerCard = cards[Math.floor(Math.random()*cards.length)]
-
-    // //shuffle the cards
-    // const shuffledCards = shuffleArray(cards);
-
-    // // computer chooses 10 card for user
-    // const userCards = shuffledCards.slice(0, 10);
-
-    // //computer chooses 10 cards for computer
-    // const computerCards = shuffledCards.slice(0, 10);
 
     res.json(computerCard);
   
